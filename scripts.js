d5dc1df24a1a83ed3dc7eb64d351892e6e77fda4
ZeroLearn <rawlingreed@gmail.com>
2023-11-26 13:28:54 -0800

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-// scripts.js
-document.addEventListener('DOMContentLoaded', (event) => {
-    console.log("Website loaded!");
-});
-
-window.addEventListener('DOMContentLoaded', (event) => {
-    filterImages('animals'); // or any default category
-});
-
