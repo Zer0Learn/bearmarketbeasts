03c7940748d086b75c62cacd6ca5c752a67e626f
ZeroLearn <rawlingreed@gmail.com>
2023-11-26 13:29:24 -0800

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// scripts.js
+document.addEventListener('DOMContentLoaded', (event) => {
+    console.log("Website loaded!");
+});
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    filterImages('animals'); // or any default category
+});
+
